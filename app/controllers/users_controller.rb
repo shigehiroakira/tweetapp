b0ecd245287576c9eb1587b177979000512f2395
shigehiroakira <shigehiroakira69@gmail.com>
2021-01-15 12:42:12 +0900

    詳細ページの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @user = User.all
   end
@@ -7,11 +8,12 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = user.nickname
     @tweets = user.tweets
+
     @following_users = current_user.following_user
     @follower_users = current_user.follower_user
+   
   end
 
   def mypage
-    
   end
 end
